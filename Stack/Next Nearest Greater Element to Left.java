8094e71e3dc8f6497a611af80b8fdf145dd434ff
Raman Bhardwaj <85386401+TimeTraveller-099@users.noreply.github.com>
2025-05-10 23:25:26 +0530

    Update Next Nearest Greater Element to Left.java

--- Stack/Next Nearest Greater Element to Left.java
+++ Stack/Next Nearest Greater Element to Left.java
@@ -1,13 +1,11 @@
-/* Given an integer array nums, return the next greater number for every element in nums. The next greater number of a number x is the first greater number to its left in the array. If it doesn't exist, return -1 for this number. */
-
 static int[] NGEL (int[] arr) {
-		Stack<Integer> stack = new Stack<>();
-		int n = arr.length;
-		int[] ans = new int[n];
+    Stack<Integer> stack = new Stack<>();
+    int n = arr.length;
+    int[] ans = new int[n];
         
-		for (int i = 0; i < n; i++) {
-				while(!stack.isEmpty() && stack.peek() <= arr[i]) {
-						stack.pop();
+    for (int i = 0; i < n; i++) {
+        while(!stack.isEmpty() && stack.peek() <= arr[i]) {
+            stack.pop();
         }
         if (stack.isEmpty()) {
             ans[i] = -1;
