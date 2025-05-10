d95070b66ed7c600352c336ddcd1f717eb76c2fe
Raman Bhardwaj <85386401+TimeTraveller-099@users.noreply.github.com>
2025-05-11 00:03:19 +0530

    Update Nearest Smallest Element to Left.java

--- Stack/Nearest Smallest Element to Left.java
+++ Stack/Nearest Smallest Element to Left.java
@@ -1,18 +1,18 @@
 private static int[] NSEL(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int n = arr.length;
-        int[] NSEL = new int[n];
+    Stack<Integer> stack = new Stack<>();
+    int n = arr.length;
+    int[] NSEL = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            while(!stack.isEmpty() && stack.peek() >= arr[i]) {
-                stack.pop();
-            }
-            if (stack.isEmpty()) {
-                NSEL[i] = -1;
-            } else {
-                NSEL[i] = stack.peek();
-            }
-            stack.push(arr[i]);
+    for (int i = 0; i < n; i++) {
+        while(!stack.isEmpty() && stack.peek() >= arr[i]) {
+            stack.pop();
         }
-        return NSEL;
+        if (stack.isEmpty()) {
+            NSEL[i] = -1;
+        } else {
+            NSEL[i] = stack.peek();
+        }
+        stack.push(arr[i]);
     }
+    return NSEL;
+}
